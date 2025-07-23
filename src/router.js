c68dd5f7ca6592f832e60d92aa58f96aa9f9c248
Johan Lindkvist <johanlindkvist89@gmail.com>
2025-07-23 20:30:45 +0200

    refactor: move to factory function

---
+++ src/router.js
@@ -0,0 +1,79 @@
+/**
+ * @typedef {{
+ *  request: Request;
+ *  params: Record<string, string | undefined>
+ * }} Context
+ */
+
+/**
+ * @typedef {(
+ *  (context: Context) => Response
+ * )} Handler
+ */
+
+/**
+ * @typedef {{
+ *  method: "GET" | "POST";
+ *  pattern: URLPattern;
+ *  handler: Handler;
+ * }} Route
+ */
+
+/**
+ * @typedef {{
+ *  get(pathname: string, handler: Handler): void;
+ *  post(pathname: string, handler: Handler): void;
+ *  handle(request: Request): Response | Promise<Response>;
+ * }} Router
+ */
+
+/**
+ * @returns {Router}
+ */
+const createRouter = () => {
+  /** @type {Route[]} */
+  const routes = [];
+
+  /**
+   * @param {string} pathname
+   * @param {Handler} handler
+   */
+  const get = (pathname, handler) => {
+    routes.push({
+      method: "GET",
+      pattern: new URLPattern({ pathname }),
+      handler,
+    });
+  };
+
+  /**
+   * @param {string} pathname
+   * @param {Handler} handler
+   */
+  const post = (pathname, handler) => {
+    routes.push({
+      method: "POST",
+      pattern: new URLPattern({ pathname }),
+      handler,
+    });
+  };
+
+  /**
+   * @param {Request} request
+   */
+  const handle = (request) => {
+    for (const route of routes) {
+      const match = route.pattern.exec(request.url);
+      if (match && request.method === route.method) {
+        const params = match.pathname.groups;
+        return route.handler({ request, params });
+      }
+    }
+
+    return new Response("Not found", { status: 404 });
+  };
+
+  return { get, post, handle };
+};
+
+export { createRouter };

d2d2f8d23b698bdd55d836d0f737c329fcca1142
Johan Lindkvist <johanlindkvist89@gmail.com>
2026-04-12 15:35:41 +0200

    feat: add filter

--- src/middlewares/serve-static.test.js
+++ src/middlewares/serve-static.test.js
@@ -70,4 +70,41 @@ describe("serveStatic", () => {
 			await rm(baseDirpath, { recursive: true, force: true });
 		}
 	});
+
+	it("should enforce the filter function and return 403 for disallowed files", async () => {
+		const dirpath = new URL("./static/", import.meta.url);
+		const allowedFilepath = new URL("./static/style.css", import.meta.url);
+		const forbiddenFilepath = new URL("./static/controller.js", import.meta.url);
+
+		await mkdir(dirpath, { recursive: true });
+		await writeFile(allowedFilepath, "css-content");
+		await writeFile(forbiddenFilepath, "secret-code");
+
+		const router = createRouter({
+			routes: [
+				serveStatic({
+					path: "/static",
+					base: import.meta.url,
+					filter: (filename) => filename.endsWith(".css"),
+				}),
+			],
+			initialState: () => ({}),
+		});
+
+		try {
+			const allowedRequest = new Request("http://localhost/static/style.css");
+			const allowedResponse = await router.handle(allowedRequest);
+
+			deepStrictEqual(allowedResponse.status, 200);
+			deepStrictEqual(await allowedResponse.text(), "css-content");
+
+			const forbiddenRequest = new Request("http://localhost/static/controller.js");
+			const forbiddenResponse = await router.handle(forbiddenRequest);
+
+			deepStrictEqual(forbiddenResponse.status, 403);
+			deepStrictEqual(await forbiddenResponse.text(), "Forbidden");
+		} finally {
+			await rm(dirpath, { recursive: true, force: true });
+		}
+	});
 });
